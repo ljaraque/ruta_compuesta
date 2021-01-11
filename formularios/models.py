d5a38268f95ab7bad88e2c4f119c48385ddba521
ljaraque <ljaraque@yahoo.com>
2021-01-11 18:27:00 -0300

    relaciones

--- formularios/models.py
+++ formularios/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Musico(models.Model):
+    nombre = models.CharField(max_length=80)
+    apellido = models.CharField(max_length=50, default="Vacante")
+    pais = models.CharField(max_length=50, default="Chile")
+
+
 class Guitarra(models.Model):
     marca = models.CharField(max_length=80)
     modelo = models.CharField(max_length=50)
     cuerdas = models.IntegerField()
     madera = models.CharField(max_length=20, default="No informada")
     fecha_compra = models.DateField()
+    musico = models.ForeignKey(Musico, on_delete=models.CASCADE, default=None)
 
-class Musico(models.Model):
+
+class Banda(models.Model):
     nombre = models.CharField(max_length=80)
-    guitarra = models.ForeignKey(Guitarra, on_delete=models.CASCADE)
+    fecha_formacion = models.DateField()
+    musicos = models.ManyToManyField(Musico)

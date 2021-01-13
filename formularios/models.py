aa63c4e871051ef319633d52ef1817ff75939518
ljaraque <ljaraque@yahoo.com>
2021-01-13 17:40:05 -0300

    variaciones de modelos para pruebas de migraciones

--- formularios/models.py
+++ formularios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Musico(models.Model):
-    nombre = models.CharField(max_length=80)
+    nombre = models.CharField(max_length=250)
     apellido = models.CharField(max_length=50, default="Vacante")
     pais = models.CharField(max_length=50, default="Chile")
 
@@ -21,3 +21,9 @@ class Banda(models.Model):
     nombre = models.CharField(max_length=80)
     fecha_formacion = models.DateField()
     musicos = models.ManyToManyField(Musico)
+
+
+class Pasaporte(models.Model):
+    codigo = models.CharField(max_length=50, unique=True, null=False)
+    fecha_expiracion = models.DateField()
+    musico = models.OneToOneField(Musico, on_delete=models.CASCADE)

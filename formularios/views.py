9daa461daa04d3e32920c711ab41f891f6a02361
ljaraque <ljaraque@yahoo.com>
2021-01-14 18:17:44 -0300

    CRUD Guitarras con DB, Create, Read y Delete

--- formularios/views.py
+++ formularios/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import PrimerFormulario
 from django.conf import settings
 import json
-from .models import Guitarra
+from .models import Guitarra, Musico
 
-# Create your views here.
+#CRUD: CREATE con archivo
 def crear_guitarra(request):
     formulario = PrimerFormulario(request.POST or None)
     context = {'form': formulario}
@@ -23,10 +23,28 @@ def crear_guitarra(request):
     return render(request, 'formularios/crear_guitarra.html', context)
 
 
-def crear_guitarra_manual(request):
+#CRUD: CREATE con Base de Datos
+def crear_guitarra_db(request):
     formulario = PrimerFormulario(request.POST or None)
     context = {'form': formulario}
     if formulario.is_valid():
+        form_data = formulario.cleaned_data
+        form_data['fecha_compra']=form_data['fecha_compra'].strftime("%Y-%m-%d")
+        musico_primero = Musico.objects.all()[0]
+        Guitarra.objects.create(
+                    marca=form_data['marca'], 
+					modelo=form_data['modelo'], 
+					cuerdas=form_data['cuerdas'], 
+					fecha_compra=form_data['fecha_compra'],
+                    musico = musico_primero
+                    )
+        return redirect('formularios:lista_guitarras_db')
+    return render(request, 'formularios/crear_guitarra_db.html', context)
+
+
+'''
+def crear_guitarra_manual(request):
+    formulario =Guitarra.is_valid():
         form_data = formulario.cleaned_data
         form_data['fecha_compra']=form_data['fecha_compra'].strftime("%Y-%m-%d")
         filename= "/formularios/static/formularios/data/guitarras.json"
@@ -39,13 +57,26 @@ def crear_guitarra_manual(request):
             json.dump(guitarras, file)
         return redirect('formularios:crear_exitoso')
     return render(request, 'formularios/crear_guitarra_manual.html', context)
+'''
 
+
+
+#CRUD: READ con archivo JSON
 def crear_exitoso(request):
     filename= "/formularios/static/formularios/data/guitarras.json"
     with open(str(settings.BASE_DIR)+filename, "r") as file:
         guitarras=json.load(file)
     return render(request, 'formularios/crear_exitoso.html', context=guitarras)
 
+
+#CRUD: READ con Base de Datos
+def lista_guitarras_db(request):
+    lista_guitarras = list(Guitarra.objects.all().values())
+    guitarras = {'guitarras': lista_guitarras}
+    return render(request, 'formularios/lista_guitarras_db.html', context=guitarras)
+
+
+#CRUD: DELETE con archivo JSON
 def eliminar_guitarra(request, id):
     if request.method == "POST":
         filename= "/formularios/static/formularios/data/guitarras.json"
@@ -61,6 +92,14 @@ def eliminar_guitarra(request, id):
         return redirect('formularios:crear_exitoso')
     context = {'id': id} 
     return render(request, "formularios/eliminar_guitarra.html", context)
+
+#CRUD: DELETE con Base de Datos
+def eliminar_guitarra_db(request, id):
+    if request.method == "POST":
+        Guitarra.objects.filter(id=id).delete()
+        return redirect('formularios:lista_guitarras_db')
+    context = {'id': id} 
+    return render(request, "formularios/eliminar_guitarra_db.html", context)
 
 def grafico2(request):
     lista = []

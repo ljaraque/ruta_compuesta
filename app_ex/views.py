baab6324883adbdf2049f9b0acb8d52fee132d8a
sezniv <sezniv@gmail.com>
2021-01-22 18:11:52 -0300

    En funcion inicio se valida si el usuario tiene cookies en la pagina y en la tabladatos se crea funcion que guarda la cantidad de sesiones que se visito el sitio

--- app_ex/views.py
+++ app_ex/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from django.conf import settings
 import csv
+import uuid
 
 # Create your views here.
 
 def inicio(request):
-    return render(
+    print("Cookies", request.COOKIES)
+    response = render(
         request, 
         'app_ex/elefante.html', 
         context={'texto':'Texto de Ejercicio 1'}
         )
+
+    if 'Identificador' not in request.COOKIES:
+        response.set_cookie(
+                    key = 'Identificador',
+                    value = uuid.uuid4(),
+                    max_age = 60*60*24*30 #seg*min*dias*mes
+        )
+
+    response.set_cookie(
+                key = 'V.I.P',
+                value = "Eres un ganador"
+        )
+    return response
+     
 
 def empresa(request):
     return render(
@@ -44,4 +60,14 @@ def tabladatos(request):
 
         datos_para_context = {'texto': 'Estos son datos de iris',
                     'data': data_list}
+
+    if 'visitas' not in request.session:
+        request.session['visitas']= 1
+    else:
+        if request.session['visitas']<10:
+            request.session['visitas'] += 1
+        else:
+            request.session['visitas'] = 1
+    datos_para_context['num_visitas']=request.session['visitas']
+
     return render(request, 'app_ex/iris.html', context = datos_para_context)

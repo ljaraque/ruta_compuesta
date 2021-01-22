807570567418582814711bde1d35f508ad74fcd8
ljaraque <ljaraque@yahoo.com>
2021-01-22 05:32:07 -0300

    cookies and sessions

--- app_ex/views.py
+++ app_ex/views.py
@@ -3,13 +3,22 @@ from django.conf import settings
 import csv
 
 # Create your views here.
-
+import uuid
 def inicio(request):
-    return render(
+    print("COOKIES: ", request.COOKIES)
+    response = render(
         request, 
         'app_ex/elefante.html', 
         context={'texto':'Texto de Ejercicio 1'}
         )
+    if 'identificador' not in request.COOKIES:
+        max_age = 365 * 24 * 60 * 60  # 1 Año
+        response.set_cookie(
+                        key="identificador", 
+                        value=uuid.uuid4(), 
+                        max_age=max_age
+                        )
+    return response
 
 def empresa(request):
     return render(
@@ -44,4 +53,12 @@ def tabladatos(request):
 
         datos_para_context = {'texto': 'Estos son datos de iris',
                     'data': data_list}
+    if 'visitas' not in request.session:
+        request.session['visitas']= 1
+    else:
+        if request.session['visitas']<10:
+            request.session['visitas'] += 1
+        else:
+            request.session['visitas'] = 1
+    datos_para_context['num_visitas']=request.session['visitas']
     return render(request, 'app_ex/iris.html', context = datos_para_context)
